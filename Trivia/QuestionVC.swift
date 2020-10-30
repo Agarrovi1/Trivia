d699358ee9e695222ff4e8769bc3f6be770d2484
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-29 21:49:03 -0400

    adds questionVC file

---
+++ Trivia/QuestionVC.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionVC.swift
+//  Trivia
+//
+//  Created by Angela Garrovillas on 10/29/20.
+//  Copyright © 2020 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class QuestionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
