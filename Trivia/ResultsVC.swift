8d71374e196e57b829f3f6d71d1ffc8b4dfeba5d
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-30 16:53:33 -0400

    add play again button

--- Trivia/ResultsVC.swift
+++ Trivia/ResultsVC.swift
@@ -12,27 +12,51 @@ class ResultsVC: UIViewController {
     //MARK: - UIObjects
     var scoreLabel: UILabel = {
         let label = UILabel()
-        label.text = "Results:\n \(TriviaModel.shared.getScore())"
-        label.numberOfLines = 2
+        label.text = "Your Score:\n \(TriviaModel.shared.getScore())"
+        label.numberOfLines = 3
         label.textAlignment = .center
         label.textColor = .black
         return label
+    }()
+    var playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play another round? ", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 3
+        button.backgroundColor = .white
+        button.titleLabel?.numberOfLines = 2
+        return button
     }()
     
     //MARK: - Constraints
     private func setAllConstraints() {
         constrainScoreLabel()
+        constrainPlayButton()
     }
     private func constrainScoreLabel() {
         view.addSubview(scoreLabel)
         scoreLabel.translatesAutoresizingMaskIntoConstraints = false
-        scoreLabel.font = UIFont.systemFont(ofSize: 25)
+        scoreLabel.font = UIFont.systemFont(ofSize: 60)
         NSLayoutConstraint.activate([
-            scoreLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: view.frame.height * 0.2),
+            scoreLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scoreLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
             scoreLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
             scoreLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+    }
+    private func constrainPlayButton() {
+        view.addSubview(playButton)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            playButton.topAnchor.constraint(equalTo: view.centerYAnchor,constant: view.frame.height * 0.05),
+            playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
+            playButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)])
+    }
+    private func changeButtonLabel() {
+        
     }
 
     //MARK: - LifeCyle
