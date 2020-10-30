8ab223ff5f3047bc1661a8cdcdc7b5f46c7291e1
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-30 11:50:34 -0400

    adds triviaModel

---
+++ Trivia/TriviaModel.swift
@@ -0,0 +1,23 @@
+//
+//  TriviaModel.swift
+//  Trivia
+//
+//  Created by Angela Garrovillas on 10/30/20.
+//  Copyright © 2020 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
+class TriviaModel {
+    static let shared = TriviaModel()
+    
+    
+    init() {
+        
+    }
+    
+    private func getQuestions() {}
+    private func makeRoundQuestions() {}
+    
+    func getCurrentQuestion() {}
+    
+}
