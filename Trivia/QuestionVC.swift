df73cfb0c36a02f823d40283d346a892bd09105c
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-30 10:21:48 -0400

    adds stackView to hold answers

--- Trivia/QuestionVC.swift
+++ Trivia/QuestionVC.swift
@@ -16,13 +16,16 @@ class QuestionVC: UIViewController {
     var answerButtonTwo = UIButton()
     var answerButtonThree = UIButton()
     
+    var answerStackView = UIStackView()
+    
     
     
     //MARK: - Functions
     private func updateButtonStyle(button: UIButton, tag: Int,title: String) {
+        view.addSubview(button)
         button.backgroundColor = .systemPink
         button.layer.cornerRadius = 10
-        button.layer.borderWidth = 1
+        button.layer.borderWidth = 2
         button.layer.borderColor = UIColor.purple.cgColor
         button.setTitleColor(.black, for: .normal)
         button.setTitle(title, for: .normal)
@@ -32,12 +35,41 @@ class QuestionVC: UIViewController {
     }
     //MARK: - Setup
     private func setupButtons() {
+        updateButtonStyle(button: answerButtonZero, tag: 0, title: "zero")
+        updateButtonStyle(button: answerButtonOne, tag: 1, title: "one")
+        updateButtonStyle(button: answerButtonTwo, tag: 2, title: "two")
+        updateButtonStyle(button: answerButtonThree, tag: 3, title: "three")
+    }
+    
+    private func addConstraints() {
+        constrainStackView()
+    }
+    
+    private func constrainStackView() {
+        answerStackView.addArrangedSubview(answerButtonZero)
+        answerStackView.addArrangedSubview(answerButtonOne)
+        answerStackView.addArrangedSubview(answerButtonTwo)
+        answerStackView.addArrangedSubview(answerButtonThree)
         
+        answerStackView.axis = .vertical
+        answerStackView.spacing = 30
+        answerStackView.distribution = .fillEqually
+        view.addSubview(answerStackView)
+        answerStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            answerStackView.topAnchor.constraint(equalTo: view.centerYAnchor),
+            answerStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            answerStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            answerStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4)])
     }
 
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupButtons()
+        constrainStackView()
 
     }
     
