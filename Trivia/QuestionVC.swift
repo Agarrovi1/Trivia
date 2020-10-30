70d99a59d04a7805aaee4f277096efef148d555d
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-29 22:32:32 -0400

    adds func to update buttons

--- Trivia/QuestionVC.swift
+++ Trivia/QuestionVC.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class QuestionVC: UIViewController {
+    //MARK: - UIObject
+    
+    var answerButtonZero = UIButton()
+    var answerButtonOne = UIButton()
+    var answerButtonTwo = UIButton()
+    var answerButtonThree = UIButton()
+    
+    
+    
+    //MARK: - Functions
+    private func updateButtonStyle(button: UIButton, tag: Int,title: String) {
+        button.backgroundColor = .systemPink
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.purple.cgColor
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.tag = tag
+    
+    }
+    //MARK: - Setup
+    private func setupButtons() {
+        
+    }
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
